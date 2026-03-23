7fcc65d8629422ded9359e98a46a722b536ff5db
aparnaakhilesh <aparnaakhileshms@gmail.com>
2026-03-23 12:47:13 +0530

    Update pipe2.py

--- pipe2.py
+++ pipe2.py
@@ -1,28 +1,7 @@
-# pipe_lower.py
+# pipe_lower_static.py
 
-def to_lowercase(input_file, output_file):
-    print(f"Reading {input_file}...")
+message = "HELLO JENKINS PIPELINE"
+print("Original:", message)
 
-    try:
-        with open(input_file, 'r') as f:
-            text = f.read()
-    except FileNotFoundError:
-        print("Input file not found.")
-        return
-
-    lower_text = text.lower()
-
-    with open(output_file, 'w') as f:
-        f.write(lower_text)
-
-    print(f"Conversion complete. Results saved to {output_file}")
-
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) != 3:
-        print("Usage: python pipe_lower.py <input_file> <output_file>")
-    else:
-        to_lowercase(sys.argv[1], sys.argv[2])
-
-
+lower_message = message.lower()
+print("Lowercase:", lower_message)
